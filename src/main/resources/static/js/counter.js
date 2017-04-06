818b7d8929c5d74efc0a762b376d38b83c12d3f4
Julian <julian.blair@consid.se>
2017-04-06 16:40:43 +0200

    added material design css pate

--- src/main/resources/static/js/counter.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-
-
-    $('#getting-started').countdown('2018/01/01', function(event) {
-        $(this).html(event.strftime('%w weeks %d days %H:%M:%S'));
-    });
-
-})
